e8b7819d39688f69a658532fee0a5436cbaddf3d
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-08-09 21:19:48 +0430

    Updates

---
+++ precache-manifest.7f6105650551e1c2ef116fc5f81b6a75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff0e9e5445559e731e9f9c414f53750",
+    "url": "/CRM/index.html"
+  },
+  {
+    "revision": "81ac0cc2c0e0d71cf8de",
+    "url": "/CRM/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "222da20a11875b8658cf",
+    "url": "/CRM/static/css/main.8414d376.chunk.css"
+  },
+  {
+    "revision": "81ac0cc2c0e0d71cf8de",
+    "url": "/CRM/static/js/2.3956f31e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CRM/static/js/2.3956f31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222da20a11875b8658cf",
+    "url": "/CRM/static/js/main.fbbc6c14.chunk.js"
+  },
+  {
+    "revision": "5b65bfc2874bc8f4ca71",
+    "url": "/CRM/static/js/runtime-main.6c408a77.js"
+  },
+  {
+    "revision": "e59513c82578df5dafb6b8da951be9b6",
+    "url": "/CRM/static/media/Yekan.e59513c8.ttf"
+  }
+]);
